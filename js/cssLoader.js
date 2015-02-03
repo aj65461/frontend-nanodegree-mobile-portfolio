e96f2d1c00a768d4848ffa731ba14e7968f61228
Aaron <aj65461@gmail.com>
2015-02-02 21:02:49 -0500

    Comment the cssLoader.js file

--- js/cssLoader.js
+++ js/cssLoader.js
@@ -1,6 +1,10 @@
+/**
+ * Source: https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery
+ * This file will be used load the full style.css once the page has rendered.
+ */
 var cb = function() {
   var l = document.createElement('link'); l.rel = 'stylesheet';
-  l.href = 'small.css';
+  l.href = 'style.css';
   var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
 };
 var raf = requestAnimationFrame || mozRequestAnimationFrame ||
